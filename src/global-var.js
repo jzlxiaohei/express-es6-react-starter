45c2f6e0ab9f151939e67add726bbfaea9959a02
zilong <jzlxiaohei@163.com>
2016-03-09 16:16:21 +0800

    fix some issue

--- src/global-var.js
+++ src/global-var.js
@@ -1,7 +1,10 @@
 var path = require("path")
 var config = require('../config')
-var ReactEngine = require("../react-view-engine",{isProduction:config.isProduction})
-var engine = new ReactEngine(path.join(__dirname,"./components"))
+var ReactEngine = require("../react-view-engine")
+var engine = new ReactEngine(
+    path.join(__dirname,"./components"),
+    {isProduction:config.isProduction}
+)
 
 global.$reactViewEngine = engine
 global.$asyncWrapper = function(fn){
